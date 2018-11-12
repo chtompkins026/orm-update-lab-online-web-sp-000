7850fd8773f028da53654ea6631e4929a1b74778
Christopher Tompkins <chtompkins026@gmail.com>
2018-11-12 20:06:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,7 @@ class Student
       DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
   
-def self.find_by_name(name)
+  def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
       FROM students
